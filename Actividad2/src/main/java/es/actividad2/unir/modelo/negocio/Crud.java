4ffcfde5161f40aa38cb7cc311c76612e0170eee
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-11-16 16:11:27 +0100

    paquetes

---
+++ Actividad2/src/main/java/es/actividad2/unir/modelo/negocio/Crud.java
@@ -0,0 +1,46 @@
+package es.actividad2.unir.modelo.negocio;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import es.actividad2.unir.modelo.entidad.Libro;
+
+//Aqui la logica de negocio
+
+@Component
+public class Crud {
+	
+	//metodos para hacer las tareas
+	//los metodos son siempre verbos
+	
+	public Libro altaLibro (Libro libro) {
+		
+		return null;
+		
+	}
+	
+	public Libro bajaLibro (int id) {
+			
+			return null;
+			
+	}
+	
+	public Libro modificarLibro (int id) {
+		
+		return null;
+		
+	}
+	
+	public Libro obtenerLibroPorId (int Id) {
+		return null;
+		
+	}
+	
+	public List<Libro> listarLibros (){
+		
+		return null;
+		
+	}
+	
+}
--- Actividad2/src/main/java/es/actividad2/unir/negocio/entidad/Libro.java
+++
@@ -1,8 +0,0 @@
-package es.actividad2.unir.negocio.entidad;
-
-public class Libro {
-	private int id;
-	private String titulo;
-	private String editorial;
-	
-}
